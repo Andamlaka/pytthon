e9933c8f182f2be30b6c709ac0447b90d1097388
Andamlaka <andargachweewawey@gmail.com>
2025-03-03 16:15:14 +0300

    package creation

---
+++ list.py
@@ -0,0 +1,13 @@
+my_list=['apple', 'orange', 'papaya', 'banana', 'mango', 'avocado']
+a=my_list[1]
+print(a)
+
+
+favorite_bookdict={'title': 'miteralion', 'author': 'alemayehu wase eshete', 'genre': 'fiction', 'year': 2022}
+print(favorite_bookdict)
+a=favorite_bookdict['genre']
+print(a)
+
+
+
+
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-my_list=['apple', 'orange', 'papaya', 'banana', 'mango', 'avocado']
-a=my_list[1]
-print(a)
+from arithmetic_operations import perfume_operation
+
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, drvide): ").strip().lower()
 
 
-favorite_bookdict={'title': 'miteralion', 'author': 'alemayehu wase eshete', 'genre': 'fiction', 'year': 2022}
-print(favorite_bookdict)
-a=favorite_bookdict['genre']
-print(a)
+    result = perfume_operation(num1, num2, operation)
+    print(f"Result: {result}")
 
-
-
-
+if __name__ == "__main__":
+    main()
